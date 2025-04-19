fa6cea5585c422dc7a983880184de8771037d3a6
dewreeh <severussenap@gmail.com>
2025-04-19 18:40:34 +0300

    added new year holidays

--- src/main/java/org/repin/service/PaymentService.java
+++ src/main/java/org/repin/service/PaymentService.java
@@ -14,7 +14,13 @@ public class PaymentService {
 
     private static final Set<MonthDay> HOLIDAYS = Set.of(
             MonthDay.of(1, 1),
+            MonthDay.of(1, 2),
+            MonthDay.of(1, 3),
+            MonthDay.of(1, 4),
+            MonthDay.of(1, 5),
+            MonthDay.of(1, 6),
             MonthDay.of(1, 7),
+            MonthDay.of(1, 8),
             MonthDay.of(2, 23),
             MonthDay.of(3, 8),
             MonthDay.of(5, 1),
